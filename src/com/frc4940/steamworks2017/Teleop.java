b07a5d3df41f474bda9cef335ae246bf9a5a1bf2
Unknown <jazzy.d@cogeco.ca>
2017-02-08 15:08:32 -0500

    added init function in teleop.java

--- src/com/frc4940/steamworks2017/Teleop.java
+++ src/com/frc4940/steamworks2017/Teleop.java
@@ -1,15 +1,22 @@
 package com.frc4940.steamworks2017;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.RobotDrive;
+
 
 
 public class Teleop {
+	
+	AnalogGyro gyroscope = new AnalogGyro(0);
 
 	DriveTrain drive = new DriveTrain();
 	
 	public void run() {
 		drive._driveRobotSQ(Xbox.getTriggerAxis(Hand.kRight), Xbox.getTriggerAxis(Hand.kLeft));
 	
+	}
+	
+	public void init() {
+		gyroscope.initGyro();
 	}
 	}
 
