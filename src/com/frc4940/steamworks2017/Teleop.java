2a1994166b4c36bef35cc5d2b18940c56cabd7e9
Unknown <jazzy.d@cogeco.ca>
2017-02-09 15:26:26 -0500

    moved gear code into correct class

--- src/com/frc4940/steamworks2017/Teleop.java
+++ src/com/frc4940/steamworks2017/Teleop.java
@@ -3,18 +3,20 @@ import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 
-
 public class Teleop {
 	
+	GearHolder gear = new GearHolder();
+	
+	//jack and D@nte was here
 	AnalogGyro gyroscope = new AnalogGyro(0);
 
 	DriveTrain drive = new DriveTrain();
 	
 	BallscrewMotors ballscrew = new BallscrewMotors(0); 
-	
+	//adding controls to drive train
 	public void run() {
 		drive._driveRobotSQ(Xbox.getTriggerAxis(Hand.kRight), Xbox.getTriggerAxis(Hand.kLeft));
-		
+		//ball screw controls(x for up, y for down)
 		if(Xbox.getXButton()){
 			ballscrew.ballsscrew(1);
 		}
@@ -27,7 +29,13 @@ public class Teleop {
 	public void init() {
 		gyroscope.initGyro();
 	}
-}
+	
+	//control to toggle gear holder on/off
+
+		}
+	
+	
+
 
 	
 	
