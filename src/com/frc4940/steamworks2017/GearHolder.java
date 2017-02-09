f1ca39570582791ff98893378d0a781f00ff246b
Unknown <jazzy.d@cogeco.ca>
2017-02-09 15:58:26 -0500

    Fixed up gear code

--- src/com/frc4940/steamworks2017/GearHolder.java
+++ src/com/frc4940/steamworks2017/GearHolder.java
@@ -1,5 +1,31 @@
 package com.frc4940.steamworks2017;
+import com.ctre.CANTalon;
+
 
 public class GearHolder {
+	
+	CANTalon gear;
 
+  boolean wasPressed = !true;	
+  boolean isOpen = false;
+		  
+	public void gearholdertoggle(boolean isPressed){
+		
+		if (wasPressed == false && isPressed == true){
+			isOpen = !isOpen;
+			
+		}
+		
+		if (isOpen == true){
+//			gear.enable();
+		} else {
+//			gear.disable();
+		}
+		
+		//|||||||||||||||||||||||||
+		wasPressed = isPressed; 
+
+	
+	
+	}
 }
