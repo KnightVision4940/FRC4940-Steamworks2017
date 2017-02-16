1c19a43e5ba563714a941b0edaa53f356cad9cec
Stew Esho <StewardEsho@gmail.com>
2017-02-16 16:03:03 -0500

    Fixed Map.java, library issues, compiling issues

--- src/com/frc4940/steamworks2017/BallscrewMotors.java
+++ src/com/frc4940/steamworks2017/BallscrewMotors.java
@@ -4,17 +4,20 @@ import edu.wpi.first.wpilibj.VictorSP;;
 
 public class BallscrewMotors {
 	
+	
+public BallscrewMotors(int CANport, int victorPort){
+//	 ballscrew = new CANTalon(CANport); 
+		 
+		 motorlauncher = new VictorSP(victorPort);
+	 }
+	
 // CANTalon ballscrew;
  VictorSP motorlauncher;
  public void ballsscrew(double speed){
 //	ballscrew.set(speed);
  }
 
- public BallscrewMotors(int CANport, int victorPort){
-// ballscrew = new CANTalon(CANport); 
-	 
-	 motorlauncher = new VictorSP(victorPort);
- }
+ 
 
  //The ball launcher motors in fort of the ballscrew
  public void Motorlauncher(double speed){
--- src/com/frc4940/steamworks2017/Teleop.java
+++ src/com/frc4940/steamworks2017/Teleop.java
@@ -1,4 +1,5 @@
 package com.frc4940.steamworks2017;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 
@@ -7,40 +8,38 @@ public class Teleop {
 	
 	GearHolder gear;
 	
-//	AnalogGyro gyroscope;
+	AnalogGyro gyroscope;
 
 	DriveTrain drive;
 	
-//	BallscrewMotors ballscrew;
-	
-	BallscrewMotors ball_launch = new BallscrewMotors(0,1);
+	BallscrewMotors ballscrew;
 //	
 	public Teleop(){
-//		gyroscope = new AnalogGyro(0);
+		gyroscope = new AnalogGyro(0);
 		drive = new DriveTrain();
-//		ballscrew = new BallscrewMotors(0);
+		ballscrew = new BallscrewMotors(0,Map.PWM.BALLSCREW_PORT);
 	}
 	
 	public void run() {
 		drive._driveRobotSQ(-Xbox.getTriggers(), Xbox.getx(Hand.kLeft));
 		
-//		if(Xbox.getXButton()){
-//			ballscrew.ballsscrew(1);
-			ball_launch.Motorlauncher(1);
+		if(Xbox.getXButton()){
+			ballscrew.ballsscrew(1);
+			ballscrew.Motorlauncher(1);
 			
-//		}
-//		else if (Xbox.getYButton()){
-//			ballscrew.ballsscrew(-1);
-			ball_launch.Motorlauncher(-1);
-//		}
-			gear.gearholdertoggle(Xbox.getAButton());
+		}
+		else if (Xbox.getYButton()){
+			ballscrew.ballsscrew(-1);
+			ballscrew.Motorlauncher(-1);
+		}
+//			gear.gearholdertoggle(Xbox.getAButton());
 	
 	}
 
 
 	
 	public void init() {
-//		gyroscope.initGyro();
+		gyroscope.initGyro();
 	}
 }
 
