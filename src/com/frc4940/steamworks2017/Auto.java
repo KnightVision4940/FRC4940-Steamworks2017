e2c64df2355126023624c1feeeafdfcf195b43dc
Stew Esho <StewardEsho@gmail.com>
2017-02-19 14:03:21 -0500

    Added delays in Auto modes

--- src/com/frc4940/steamworks2017/Auto.java
+++ src/com/frc4940/steamworks2017/Auto.java
@@ -16,6 +16,8 @@ public class Auto {
 		Timer.delay(7);
 	}
 	
+	//sp00ky :O
+	
 	int autoMode = 0; 
 	
 	public void run(){
@@ -27,22 +29,21 @@ public class Auto {
 			Timer.delay(3);
 			drive._driveRobot(1, -1);
 			Timer.delay(10);
-			ballscrewup.ballsscrew(1);
+			ballscrewup.ballsscrew(1/4);
 			ballscrewup.Motorlauncher1(1);
 			ballscrewup.Motorlauncher2(1);
+			Timer.delay(5);
 		}
-
-		
-
 
 		else if (autoMode == 2){
 			drive._driveRobot(1, 0);
 			Timer.delay(3);
 			drive._driveRobot(1, -1);
 			Timer.delay(5);
-			ballscrewup.ballsscrew(1);
+			ballscrewup.ballsscrew(1/4);
 			ballscrewup.Motorlauncher1(1);
 			ballscrewup.Motorlauncher2(1);
+			Timer.delay(5);
 		}
 		else if (autoMode == 3){
 			drive._driveRobot(1, 0);
@@ -52,6 +53,7 @@ public class Auto {
 			ballscrewup.ballsscrew(1);
 			ballscrewup.Motorlauncher1(1);
 			ballscrewup.Motorlauncher2(1);
+			Timer.delay(5);
 		}
 		else if (autoMode == 4){
 			drive._driveRobot(1, 0);
@@ -70,6 +72,7 @@ public class Auto {
 			ballscrewup.ballsscrew(1);
 			ballscrewup.Motorlauncher1(1);
 			ballscrewup.Motorlauncher2(1);
+			Timer.delay(5);
 		}
 		else if (autoMode == 6){
 			drive._driveRobot(1, 0);
@@ -78,7 +81,8 @@ public class Auto {
 			Timer.delay(3);
 			ballscrewup.ballsscrew(1);
 			ballscrewup.Motorlauncher1(1);
-			ballscrewup.Motorlauncher2(1);		
+			ballscrewup.Motorlauncher2(1);
+			Timer.delay(5);
 	    }
 		else if (autoMode == 7){ 
 			
@@ -91,87 +95,88 @@ public class Auto {
 
 	
 	//ball points left
+//*
+	//public void leftbolierfar(){
+		//drive._driveRobot(1, 0);
+		//Timer.delay(3);
+		//drive._driveRobot(1, -1);
+		//Timer.delay(10);
+		//ballscrewup.ballsscrew(1);
+		//ballscrewup.Motorlauncher1(1);
+		//ballscrewup.Motorlauncher2(1);
+		//Timer.delay(5);
 
-	public void leftbolierfar(){
-		drive._driveRobot(1, 0);
-		Timer.delay(3);
-		drive._driveRobot(1, -1);
-		Timer.delay(10);
-		ballscrewup.ballsscrew(1);
-		ballscrewup.Motorlauncher1(1);
-		ballscrewup.Motorlauncher2(1);
-		Timer.delay(5);
-
-	}
+	//}
 
 
-	public void leftboliermid(){
-		drive._driveRobot(1, 0);
-		Timer.delay(3);
-		drive._driveRobot(1, -1);
-		Timer.delay(5);
-		ballscrewup.ballsscrew(1);
-		ballscrewup.Motorlauncher1(1);
-		ballscrewup.Motorlauncher2(1);
-		Timer.delay(5);
-	}
+	//public void leftboliermid(){
+		//drive._driveRobot(1, 0);
+		//Timer.delay(3);
+		//drive._driveRobot(1, -1);
+		//Timer.delay(5);
+		//ballscrewup.ballsscrew(1);
+		//ballscrewup.Motorlauncher1(1);
+		//ballscrewup.Motorlauncher2(1);
+		//Timer.delay(5);
+	//}
 	
 	
 
-	public void leftbolierclose(){
-		drive._driveRobot(1, 0);
-		Timer.delay(3);
-		drive._driveRobot(1, -1);
-		Timer.delay(3);
-		ballscrewup.ballsscrew(1);
-		ballscrewup.Motorlauncher1(1);
-		ballscrewup.Motorlauncher2(1);
-		Timer.delay(5);
-	}
+	//public void leftbolierclose(){
+		//drive._driveRobot(1, 0);
+		//Timer.delay(3);
+		//drive._driveRobot(1, -1);
+		//Timer.delay(3);
+		//ballscrewup.ballsscrew(1);
+		//ballscrewup.Motorlauncher1(1);
+		//ballscrewup.Motorlauncher2(1);
+		//Timer.delay(5);
+	//}
 
 
-	public void rightbolierfar(){
-		drive._driveRobot(1, 0);
-		Timer.delay(3);
-		drive._driveRobot(1, 1);
-		Timer.delay(10);
-		ballscrewup.ballsscrew(1);
-		ballscrewup.Motorlauncher1(1);
-		ballscrewup.Motorlauncher2(1);
-		Timer.delay(5);
-	}
+	//public void rightbolierfar(){
+		//drive._driveRobot(1, 0);
+		//Timer.delay(3);
+		//drive._driveRobot(1, 1);
+		//Timer.delay(10);
+		//ballscrewup.ballsscrew(1);
+		//ballscrewup.Motorlauncher1(1);
+		//ballscrewup.Motorlauncher2(1);
+		//Timer.delay(5);
+	//}
 	
-	public void rightboliermid(){
-		drive._driveRobot(1, 0);
-		Timer.delay(3);
-		drive._driveRobot(1, 1);
-		Timer.delay(5);
-		ballscrewup.ballsscrew(1);
-		ballscrewup.Motorlauncher1(1);
-		ballscrewup.Motorlauncher2(1);
-		Timer.delay(5);
-	}
+	//public void rightboliermid(){
+		//drive._driveRobot(1, 0);
+		//Timer.delay(3);
+		//drive._driveRobot(1, 1);
+		//Timer.delay(5);
+		//ballscrewup.ballsscrew(1);
+		//ballscrewup.Motorlauncher1(1);
+		//ballscrewup.Motorlauncher2(1);
+		//Timer.delay(5);
+	//}
 	
-	public void rightbolierclose(){
-		drive._driveRobot(1, 0);
-		Timer.delay(3);
-		drive._driveRobot(1, 1);
-		Timer.delay(3);
-		ballscrewup.ballsscrew(1);
-		ballscrewup.Motorlauncher1(1);
-		ballscrewup.Motorlauncher2(1);
-		Timer.delay(5);
-	}
+	//public void rightbolierclose(){
+		//drive._driveRobot(1, 0);
+		//Timer.delay(3);
+		//drive._driveRobot(1, 1);
+		//Timer.delay(3);
+		//ballscrewup.ballsscrew(1);
+		//ballscrewup.Motorlauncher1(1);
+		//ballscrewup.Motorlauncher2(1);
+		//Timer.delay(5);
+	//}
 	
 	
 	//gear points
-	public void gearOne(){
-		drive._driveRobot(1, 1);
-		Timer.delay(5);
-	}
-		public void geartwo(){
-			drive._driveRobot(1,-1);
-		}
+	//public void gearOne(){
+		//drive._driveRobot(1, 1);
+		//Timer.delay(5);
+	//}
+		//public void geartwo(){
+			//drive._driveRobot(1,-1);
+		//}
+		
 	
 }		
 		
