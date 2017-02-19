e30f062c8a9b1565d088616607825601a6294acc
Unknown <jazzy.d@cogeco.ca>
2017-02-19 13:01:20 -0500

    auto jack fix plz

--- src/com/frc4940/steamworks2017/Auto.java
+++ src/com/frc4940/steamworks2017/Auto.java
@@ -7,7 +7,7 @@ public class Auto {
 	
 	DriveTrain drive = new DriveTrain();
 	GearHolder gear = new GearHolder();
-<<<<<<< HEAD
+
 	BallscrewMotors ballscrewup = new BallscrewMotors(1,2,3);
 	BallscrewMotors launchermotors = new BallscrewMotors(1,2,3);
 	
@@ -15,7 +15,7 @@ public class Auto {
 	public void driveForward(){
 		drive._driveRobot(1, 0);
 		Timer.delay(7);
-=======
+
 	BallscrewMotors ballscrewup = new BallscrewMotors(1,2, 1);
 	BallscrewMotors launchermotors = new BallscrewMotors(1,2, 1);
 	
@@ -34,11 +34,11 @@ public class Auto {
 			launchermotors.Motorlauncher1(1);
 			launchermotors.Motorlauncher2(1);
 		}
-<<<<<<< HEAD
-		else if (autoMode == 2)
->>>>>>> dev
+
+		else if (autoMode == 2);
+
 	}
-=======
+
 		else if (autoMode == 2){
 			drive._driveRobot(1, 0);
 			Timer.delay(3);
@@ -90,12 +90,11 @@ public class Auto {
 		else if (autoMode == 8){
 			
 		}	
->>>>>>> dev
-    
+
 
 	
 	//ball points left
-<<<<<<< HEAD
+
 	public void leftbolierfar(){
 		drive._driveRobot(1, 0);
 		Timer.delay(3);
@@ -132,46 +131,8 @@ public class Auto {
 		launchermotors.Motorlauncher2(1);
 		Timer.delay(5);
 	}
-=======
-	//public void leftbolierfar(double _Speed,  double _Turn){
-		//drive._driveRobot(1, 0);
-		//Timer.delay(3);
-		//drive._driveRobot(1, -1);
-		//Timer.delay(10);
-		//ballscrewup.ballsscrew(1);
-		//launchermotors.Motorlauncher1(1);
-		//launchermotors.Motorlauncher2(1);
-		
-//}
 
 
-	//public void leftboliermid(double _Speed,  double _Turn){
-		//drive._driveRobot(1, 0);
-		//Timer.delay(3);
-		//drive._driveRobot(1, -1);
-		//Timer.delay(5);
-		//ballscrewup.ballsscrew(1);
-		//launchermotors.Motorlauncher1(1);
-		//launchermotors.Motorlauncher2(1);
-//}
-	
-	
-
-	//public void leftbolierclose(double _Speed,  double _Turn){
-		//drive._driveRobot(1, 0);
-		//Timer.delay(3);
-		//drive._driveRobot(1, -1);
-		//Timer.delay(3);
-		//ballscrewup.ballsscrew(1);
-		//launchermotors.Motorlauncher1(1);
-		//launchermotors.Motorlauncher2(1);
-//}
->>>>>>> dev
-	
-
-	
-//right ball points
-<<<<<<< HEAD
 	public void rightbolierfar(){
 		drive._driveRobot(1, 0);
 		Timer.delay(3);
@@ -210,46 +171,6 @@ public class Auto {
 	public void gearOne(){
 		drive._driveRobot(1, 1);
 		Timer.delay(5);
-=======
-	//public void rightbolierfar(double _Speed,  double _Turn){
-		//drive._driveRobot(1, 0);
-		//Timer.delay(3);
-		//drive._driveRobot(1, 1);
-		//Timer.delay(10);
-		//ballscrewup.ballsscrew(1);
-		//launchermotors.Motorlauncher1(1);
-		//launchermotors.Motorlauncher2(1);
-}
-	
-	//public void rightboliermid(double _Speed,  double _Turn){
-		//drive._driveRobot(1, 0);
-		//Timer.delay(3);
-		//drive._driveRobot(1, 1);
-		//Timer.delay(5);
-		//ballscrewup.ballsscrew(1);
-		//launchermotors.Motorlauncher1(1);
-		//launchermotors.Motorlauncher2(1);
-//}
-	
-	//public void rightbolierclose(double _Speed,  double _Turn){
-		//drive._driveRobot(1, 0);
-		//Timer.delay(3);
-		//drive._driveRobot(1, 1);
-		//Timer.delay(3);
-		//ballscrewup.ballsscrew(1);
-		//launchermotors.Motorlauncher1(1);
-		//launchermotors.Motorlauncher2(1);
-//}
-	
-	
-	//gear points
-	//public void gearOne(double _Speed,double _Turn){
-		//drive._driveRobot(1, 1);}
-
-		//public void geartwo(double _Speed,double _Turn){
-			//drive._driveRobot(1,-1);
-			
->>>>>>> dev
 	}
 		public void geartwo(){
 			drive._driveRobot(1,-1);
