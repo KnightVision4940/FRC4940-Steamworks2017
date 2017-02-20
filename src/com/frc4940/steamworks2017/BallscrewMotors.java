d6c4586d5f9a5c9242b706b2cf4a9c5f92df164d
Stew Esho <StewardEsho@gmail.com>
2017-02-20 11:06:48 -0500

    Added ballscrew limitswitch

--- src/com/frc4940/steamworks2017/BallscrewMotors.java
+++ src/com/frc4940/steamworks2017/BallscrewMotors.java
@@ -1,23 +1,38 @@
 package com.frc4940.steamworks2017;
 import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.VictorSP;;
 
 public class BallscrewMotors {
 	
+	 CANTalon ballscrew;
+	 VictorSP motorlauncher;
+	 VictorSP motorLauncher2;
+	 DigitalInput limitswitch;
+	 
 	
-public BallscrewMotors(int CANport, int victorPort, int victorPort2){
-	 ballscrew = new CANTalon(CANport); 
-		 
+	public BallscrewMotors(int CANport, int victorPort, int victorPort2){
+		 ballscrew = new CANTalon(CANport); 
+			 
 		 motorlauncher = new VictorSP(victorPort);
 		 motorLauncher2 = new VictorSP(victorPort2);
+		 limitswitch  = new DigitalInput (0);
 	 }
 	
- CANTalon ballscrew;
- VictorSP motorlauncher;
- VictorSP motorLauncher2;
+
+ 
+ 
  public void ballsscrew(double speed){
 	ballscrew.set(speed/4);
  }
+ 
+ public void setSpeed (double speed){
+		
+		 if (limitswitch.get()) {
+			ballscrew.set(0);
+		}
+	}
  
  public void ScrewEncoder(){
 	 ballscrew.getEncPosition();
